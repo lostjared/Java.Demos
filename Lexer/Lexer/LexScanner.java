e5e9f5756d5372b40f7227a016d48c485ac24058
Jared Bruni <jaredbruni@gmail.com>
2021-09-17 13:26:59 -0700

    use switch

--- Lexer/Lexer/LexScanner.java
+++ Lexer/Lexer/LexScanner.java
@@ -266,17 +266,21 @@ public class LexScanner {
     }
 
     public static void main(String args[]) {
-        if(args.length == 0) {
-            repl();
-            System.exit(0);
-        } else if(args.length == 1) {
-            lexFile(args[0], null);
-            System.exit(0);
-        } else if(args.length == 2) {
-            lexFile(args[0], args[1]);
-            System.exit(0);
+        switch(args.length) {
+            case 0:
+               repl();
+                System.exit(0);
+            break;
+            case 1:
+                lexFile(args[0], null);
+                System.exit(0);
+            break;
+            case 2:
+                lexFile(args[0], args[1]);
+                System.exit(0);
+            break;
         }
-    }
+   }
 
     public static void repl() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
